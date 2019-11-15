0ed3de023834ee027c1e40bef6f5a0b7bd3676fd
William Huang <will.h86@gmail.com>
2019-11-14 22:53:56 -0800

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use actix_files as fs;
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use listenfd::ListenFd;
-use actix_files as fs;
 // use std::path::PathBuf;
 
 fn index(_req: HttpRequest) -> impl Responder {
@@ -14,10 +14,11 @@ fn index(_req: HttpRequest) -> impl Responder {
 
 fn main() {
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new()
-    .route("/", web::get().to(index))
-    .service(fs::Files::new("/static", ".").show_files_listing())
-);
+    let mut server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .service(fs::Files::new("/static", ".").show_files_listing())
+    });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
